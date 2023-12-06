4de6f69986ee867b9e40cef7cd3e9d68de21ff82
Satya Inumarthi <satyainumarthi@outlook.com>
2023-12-07 01:18:23 +0530

    Checking in CRUD-GenericRepositotryPattern-V1

---
+++ CRUDAPI's/Program.cs
@@ -0,0 +1,49 @@
+
+using BusinessLogicLayer;
+using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace CRUDAPI_s
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseInMemoryDatabase("DatabaseContextNew");
+            });
+
+            builder.Services.AddTransient<IEmployeeService, EmployeeService>();
+            builder.Services.AddTransient<IRepository<Employee>, Repository<Employee>>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
